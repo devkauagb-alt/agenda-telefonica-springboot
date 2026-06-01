d2c0732c4fdc2526fff9758e7a5d5968a7a91264
devkauagb-alt <devkauagb@gmail.com>
2026-06-01 10:00:02 -0300

    Add files via upload

---
+++ agendaTelefonica/src/main/java/com/example/agendaTelefonica/model/Pessoa.java
@@ -0,0 +1,35 @@
+package com.example.agendaTelefonica.model;
+
+public class Pessoa {
+
+    private long id;
+    private String nome;
+    private String telefone;
+  
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+ 
+}
